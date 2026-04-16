91d3a28d836fe1a26ce01f79b8d08cd27c2ef198
Shaten21 <112821027+Shaten21@users.noreply.github.com>
2026-04-16 18:13:04 +0300

    fixid path image

--- js/components/render-card.js
+++ js/components/render-card.js
@@ -18,7 +18,7 @@ export function createProductCard(product) {
     card.innerHTML = `
     <div class="product-card">
         <div class="product-card__visual">
-            <img class="product-card__img" src="${product.image}" height="436" width="290" alt="${product.name}">
+            <img class="product-card__img" src="images/${product.image}" height="436" width="290" alt="${product.name}">
             <div class="product-card__more">
                 <a href="#" class="product-card__link btn btn--icon product-basket" id="${product.id}">
                     <span class="btn__text">В корзину</span>
